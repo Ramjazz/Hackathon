444ec1c65ebbfb0ad5563d520db55cafffa6b15f
Shrey P <32659106+ShreyP54@users.noreply.github.com>
2020-08-23 00:11:19 -0400

    updated StartScreen and DesktopLauncher

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Memeler";
+		config.useGL30 = false;
+		config.width = 640;
+		config.height = 480;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
