ed848c0a83edea25bf918d5e929d85e14507d302
Ramjazz <arjunjazz11@gmail.com>
2020-08-22 23:14:35 -0400

    Pushing

---
+++ core/src/com/mygdx/game/Verbler.java
@@ -0,0 +1,56 @@
+package com.mygdx.game;
+//Verbler Class
+//Aniruddh and Nihar
+//8.22.20
+//Purpose: To prompt the verbler, take in user input, and cross-check with the correct answer
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Verbler{
+    static String[] verblerWords = {"Insert Verbler", "Insert Verbler", "Insert Verbler", "Insert Verbler"};
+    public static int Score = 0;
+
+    public Verbler(){
+
+    }
+
+    public void performLogic(){
+        Random random = new Random();
+        int index = random.nextInt(verblerWords.length);
+        System.out.println(verblerWords[index]);
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String userInput= sc.nextLine();
+        System.out.print("You have entered: "+userInput);
+
+        userInput = userInput.replaceAll("\\s+", "").toLowerCase();
+        userInput = userInput.toLowerCase();
+
+        String answer = "Answer";
+
+        if(checkVerbler(answer.toLowerCase(), userInput)) {
+            System.out.println("Congrats you have answered correctly!");
+            Score += 10;
+        }
+        else {
+            System.out.println("OH NO! Try again!");
+            if(Score >= 10)
+                Score-= 10;
+            else
+                Score += 0;
+        }
+    }
+
+
+
+
+
+
+
+    public static boolean checkVerbler(String verbler, String userEnteredWord){
+        return verbler.equals(userEnteredWord);
+    }
+}
